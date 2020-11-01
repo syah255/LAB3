1dfb96599a32e07c6e8be0cce54fa7a9313c178d
nafan <https://github.com/syah255>
2020-11-01 09:42:11 +0000

    3.1.c

--- 3.1.c
+++ 3.1.c
@@ -7,25 +7,30 @@
 int main(void)
 {
 
-  void sigint_handler(int sig);
+  void sig_handler(int sig);
   char s[200];
+   
+    if(signal(SIGINT, sig_handler) == SIG_ERR || signal(SIGQUIT, sig_handler) == SIG_ERR || signal(SIGTSTP, sig_handler) == SIG_ERR) {
+        perror("signal");
+        exit(1);
+    }
 
-  if (signal(SIGINT, sigint_handler) == SIG_ERR){
-     perror("signal");
-     exit(1);
-  }
-  printf("Enter a string:\n");
+   printf("Enter a string:\n");
 
-  if (fgets(s, 200, stdin) == NULL)
-      perror("gets");
-  else
-      printf("you entered: %s\n", s);
-
-  return 0;
+    if(fgets(s, 200, stdin) == NULL)
+        perror("gets");
+    else
+        printf("You entered %s\n", s);
+    return 0;
 }
 
-void sigint_handler(int sig)
-{
-  printf("Jangan Kacau saya! \n");
-}
 
+void sig_handler(int sig) {
+    printf("%d", sig);
+    if(sig == 2)
+        printf("This is a special signal handler for SIGINT\n");
+    else if(sig == 3)
+        printf("This is a special signal handler for SIGQUIT\n");
+    else
+        printf("This is a special signal handler for SIGTSTP\n");
+}
